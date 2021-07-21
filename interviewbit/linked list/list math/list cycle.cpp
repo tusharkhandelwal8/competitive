69e022e0e973acc127affab01352897051950755
Tushar Khandelwal <64364243+tusharkhandelwal8@users.noreply.github.com>
2021-07-22 01:11:11 +0530

    Create list cycle.cpp

---
+++ interviewbit/linked list/list math/list cycle.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::detectCycle(ListNode* A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+
+    ListNode* slow=A;
+    ListNode* fast=A;
+
+    if(A==NULL||A->next==NULL||A->next->next==NULL)
+        {
+            return NULL;
+        }
+    while((slow!=fast)&&fast&&fast->next&&fast->next->next)
+        {
+            
+                slow=slow->next;
+                fast=fast->next->next;
+                
+        }
+    if(fast==NULL||fast->next==NULL||fast->next->next==NULL)
+        return NULL;
+        
+    slow=A;
+    while(slow!=fast)
+        {
+            slow=slow->next;
+            fast=fast->next;
+        }
+    return slow;    
+}
