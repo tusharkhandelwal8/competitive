d0a74ee861f2fadb331298c86a04c60353f14cf5
Tushar Khandelwal <64364243+tusharkhandelwal8@users.noreply.github.com>
2021-08-14 10:37:49 +0530

    Create sum.cpp

---
+++ segment_tree/efficient/sum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5;  // limit for array size
+int n;  // array size
+int t[2 * N];
+
+void build() {  // build the tree
+  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+}
+
+void modify(int p, int value) {  // set value at position p
+  for (t[p += n] = value; p /= 2; ) t[p] = t[p * 2] + t[p * 2 + 1];
+}
+
+int query(int l, int r) {  // sum on interval [l, r)
+  int res = 0;
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+    if (l&1) res += t[l++];
+    if (r&1) res += t[--r];
+  }
+  return res;
+}
+
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
+  build();
+ // modify(0, 1);
+  printf("%d\n", query(2, 6)); // query [a,b)  starts from index 0 to n-1
+  return 0;
+}
